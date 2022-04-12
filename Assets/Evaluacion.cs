490f2c98c78830e3c6fb20f283ce498c0c91705f
Manuel Magni <61920152+manuZX12@users.noreply.github.com>
2022-04-12 15:19:26 -0300

    Point 2 done

--- Assets/Evaluacion.cs
+++ Assets/Evaluacion.cs
@@ -11,19 +11,34 @@ public class Evaluacion : MonoBehaviour
     int camporecaudado;
     int platearecaudada;
     int dinerorecaudado;
+    int plateasobrante;
+    int camposobrante;
     void Start()
     {
+        plateasobrante = (16200 - entradasplatea);
+        camposobrante = (20400 - entradascampo);
         if (entradascampo > 20400 || entradasplatea > 16200)
         {
             Debug.Log("El numero ingresado de entradas es incorrecto");
         }
-        else
+        else if (plateasobrante > 0 || camposobrante > 0)
         {
             camporecaudado = (entradascampo * 1200);
             platearecaudada = (entradasplatea * 2000);
             dinerorecaudado = (camporecaudado + platearecaudada);
             Debug.Log("El dinero recuado por las entradas en platea es de " + platearecaudada + ". El dinero" +
                 " recuadado por las entradas en el campo es de " + camporecaudado + " y el total recaudado es de " + dinerorecaudado + "");
+            Debug.Log("Quedaron " + plateasobrante + " vacantes en platea y " + camposobrante + " vacantes en el campo.");
+            
+        }
+        else if (plateasobrante == 0 || camposobrante == 0)
+        {
+            camporecaudado = (entradascampo * 1200);
+            platearecaudada = (entradasplatea * 2000);
+            dinerorecaudado = (camporecaudado + platearecaudada);
+            Debug.Log("El dinero recuado por las entradas en platea es de " + platearecaudada + ". El dinero" +
+                " recuadado por las entradas en el campo es de " + camporecaudado + " y el total recaudado es de " + dinerorecaudado + "");
+            Debug.Log("Sold Out");
         }
     }
 
