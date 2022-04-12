ccd5294f297001a2bc0089e8a25a9e24cdc81e9a
Manuel Magni <61920152+manuZX12@users.noreply.github.com>
2022-04-12 15:33:01 -0300

    point 4 done

--- Assets/Evaluacion.cs
+++ Assets/Evaluacion.cs
@@ -6,22 +6,22 @@ public class Evaluacion : MonoBehaviour
 {
     public int entradasplatea;
     public int entradascampo;
-    int preciocampo = 1200;
-    int precioplatea = 2000;
     int camporecaudado;
     int platearecaudada;
     int dinerorecaudado;
     int plateasobrante;
     int camposobrante;
+    int entradastotales;
     void Start()
     {
         plateasobrante = (16200 - entradasplatea);
-        camposobrante = (20400 - entradascampo);
-        if (entradascampo > 20400 || entradasplatea > 16200)
+        camposobrante = (20400 -  entradascampo);
+        entradastotales = (entradascampo + entradasplatea);
+        if (entradascampo > 20400 || entradasplatea > 16200 || entradasplatea <= -1 || entradascampo <= -1)
         {
             Debug.Log("El numero ingresado de entradas es incorrecto");
         }
-        else if (plateasobrante > 0 || camposobrante > 0)
+        else if (plateasobrante > 0 || camposobrante > 0 || entradastotales >= 18300)
         {
             camporecaudado = (entradascampo * 1200);
             platearecaudada = (entradasplatea * 2000);
@@ -29,7 +29,17 @@ public class Evaluacion : MonoBehaviour
             Debug.Log("El dinero recuado por las entradas en platea es de " + platearecaudada + ". El dinero" +
                 " recuadado por las entradas en el campo es de " + camporecaudado + " y el total recaudado es de " + dinerorecaudado + "");
             Debug.Log("Quedaron " + plateasobrante + " vacantes en platea y " + camposobrante + " vacantes en el campo.");
-            
+            Debug.Log("El festival fue un éxito!");
+        }
+        else if (plateasobrante > 0 || camposobrante > 0 || entradastotales < 18300)
+        {
+            camporecaudado = (entradascampo * 1200);
+            platearecaudada = (entradasplatea * 2000);
+            dinerorecaudado = (camporecaudado + platearecaudada);
+            Debug.Log("El dinero recuado por las entradas en platea es de " + platearecaudada + ". El dinero" +
+                " recuadado por las entradas en el campo es de " + camporecaudado + " y el total recaudado es de " + dinerorecaudado + "");
+            Debug.Log("Quedaron " + plateasobrante + " vacantes en platea y " + camposobrante + " vacantes en el campo.");
+            Debug.Log("Debemos mejorar la convocatoria");
         }
         else if (plateasobrante == 0 || camposobrante == 0)
         {
